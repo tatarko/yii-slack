5cdc486bebb597eb28cab3b4e09177a88dfe5f13
Tomáš Tatarko <tomas.tatarko@websupport.sk>
2015-03-22 12:18:47 +0100

    Initial code

---
+++ src/ApplicationComponent.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ * This file is part of the YiiSlack package.
+ *
+ * (c) Tomáš Tatarko <tomas@tatarko.sk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tatarko\YiiSlack;
+
+use Yii;
+use CApplicationComponent;
+use CException;
+use GuzzleHttp\Client;
+
+/**
+ * Slack application component
+ *
+ * Yii application component for accessing Slack API
+ * via Guzzle connection interface.
+ *
+ * @author Tomáš Tatarko <tomas@tatarko.sk>
+ */
+class ApplicationComponent extends CApplicationComponent
+{
+    /**
+     * Your application's ID
+     * @var string
+     */
+    public $appId;
+
+    /**
+     * Your application's secret code
+     * @var string
+     */
+    public $appSecret;
+
+    /**
+     * Name of the user's state to store access token
+     * @var string
+     */
+    public $userTokenState = 'slack.access.token';
+
+    /**
+     * Company's global access token
+     * @var string
+     */
+    public $companyToken;
+
+    /**
+     * Instance of Guzzle connection
+     * @var \GuzzleHttp\Client
+     */
+    private $_connection;
+
+    /**
+     * Slack Application Component initialization
+     */
+    public function init()
+    {
+        $this->_connection = new Client([
+            'base_url' => 'https://slack.com/api/',
+        ]);
+        parent::init();
+    }
+
+    /**
+     * Gets Guzzle connection instance
+     *
+     * @return \GuzzleHttp\Client
+     * @throws \CException
+     */
+    public function getConnection()
+    {
+        if(!$this->isInitialized) {
+            throw new CException('Slack application no initialized yet', 0);
+        }
+
+        return $this->_connection;
+    }
+
+    /**
+     * Gets access token
+     *
+     * This method tries to fetch access token from user's state
+     * with fallback to default access token of application
+     *
+     * @return string
+     */
+    public function getAccessToken()
+    {
+        return Yii::app()->user->getState(
+            $this->userTokenState,
+            $this->companyToken
+        );
+    }
+
+    /**
+     * Makes GET request to requested API method
+     *
+     * @param string $method Method name to call
+     * @param array $data Method input arguments
+     * @return array Parsed json from response
+     */
+    public function get($method, array $data = array())
+    {
+        return $this->getConnection()->get($method, [
+            'query' => $data + ['token' => $this->getAccessToken()]
+        ])->json();
+    }
+
+    /**
+     * Makes POST request to requested API method
+     *
+     * @param string $method Method name to call
+     * @param array $data Method input arguments
+     * @return array Parsed json from response
+     */
+    public function post($method, array $data = array())
+    {
+        return $this->getConnection()->post($method, [
+            'query' => $data + ['token' => $this->getAccessToken()]
+        ])->json();
+    }
+}
---
+++ src/AuthenticationAction.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the YiiSlack package.
+ *
+ * (c) Tomáš Tatarko <tomas@tatarko.sk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tatarko\YiiSlack;
+
+use Yii;
+use CAction;
+use CException;
+use CLogger;
+use GuzzleHttp\Exception\TransferException;
+
+/**
+ * Slack authentication action
+ *
+ * Generic action that can be assigned into any Yii controller and used
+ * for authentication. It stores fetched access token in user's state
+ * defined in Slack's application component.
+ *
+ * @author Tomáš Tatarko <tomas@tatarko.sk>
+ */
+class AuthenticationAction extends CAction
+{
+    /**
+     * Names of the application component to use
+     * @var string
+     */
+    public $componentId = 'slack';
+
+    /**
+     * Arguments that should be passed to redirect after authentication
+     * @var array
+     */
+    public $successUrl = ['/'];
+
+    /**
+     * Arguments that should be passed to redirect after authentication
+     * @var array
+     */
+    public $errorUrl = ['/'];
+
+    /**
+     * List of scopes that user should be requested for
+     * @var array
+     */
+    public $scopes = ['read'];
+
+    /**
+     * Gets Slack application component
+     *
+     * @return ApplicationComponent
+     * @throws \CException
+     */
+    public function getSlackComponent()
+    {
+        $component = Yii::app()->getComponent($this->componentId);
+        if(!$component instanceof ApplicationComponent) {
+            throw new CException('Invalid instance of Slack component', 0);
+        }
+        return $component;
+    }
+
+    /**
+     * Authentication Action
+     *
+     * @param string $code Code used for getting access_token from Slack
+     * @param string $error Responsed error message
+     */
+    public function run($code = null, $error = null)
+    {
+        if($code) {
+            $this->catchCode($code);
+        } elseif($error) {
+            Yii::log($error, $error == 'invalid_code' ? CLogger::LEVEL_WARNING : CLogger::LEVEL_ERROR, 'slack');
+            Yii::app()->redirect($this->errorUrl);
+        }
+
+        Yii::app()->redirect(
+            'https://slack.com/oauth/authorize?' . http_build_query([
+                'client_id' => $this->getSlackComponent()->appId,
+                'redirect_uri' => Yii::app()->createAbsoluteUrl(''),
+                'scope' => implode(',', $this->scopes),
+                'state' => Yii::app()->session->id,
+            ])
+        );
+    }
+
+    /**
+     * Fetchs access token and stores it into user's state
+     *
+     * @param string $code
+     */
+    protected function catchCode($code)
+    {
+        try {
+            $slack = $this->getSlackComponent();
+            $response = $slack->get('oauth.access', [
+                'client_id' => $slack->appId,
+                'client_secret' => $slack->appSecret,
+                'code' => $code,
+                'redirect_uri' => Yii::app()->createAbsoluteUrl(''),
+            ]);
+            Yii::app()->user->setState(
+                $slack->userTokenState,
+                $response['access_token']
+            );
+            Yii::app()->redirect($this->successUrl);
+        } catch(TransferException $ex) {
+            Yii::log($ex, CLogger::LEVEL_ERROR, 'slack');
+            Yii::app()->redirect($this->errorUrl);
+        }
+    }
+}
